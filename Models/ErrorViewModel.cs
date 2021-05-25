b8479b064dafd0f8b293fcb50fa4df7f39a38df4
anvorguesa <srliposa@gmail.com>
2021-05-25 17:31:13 -0500

    Base new mvc

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PC03.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
