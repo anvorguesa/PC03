7bf1f69f8e387b52646473497cfe2ece80be5767
anvorguesa <srliposa@gmail.com>
2021-05-25 17:47:43 -0500

    Model Producto

---
+++ Models/Producto.cs
@@ -0,0 +1,11 @@
+namespace PC03.Models
+{
+    public class Producto
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Foto { get; set; }
+        public string Descripcion { get; set; }
+        public int Precio { get; set; }
+    }
+}
