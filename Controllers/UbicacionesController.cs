ccb3f31f1e1e103f863ddfbcd40770ac78754a52
anvorguesa <srliposa@gmail.com>
2021-05-25 19:24:54 -0500

    Clientestabla

--- Controllers/UbicacionesController.cs
+++ Controllers/UbicacionesController.cs
@@ -1,7 +1,20 @@
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using PC03.Models;
+
 namespace PC03.Controllers
 {
-    public class UbicacionesController
+    public class UbicacionesController : Controller
     {
-        
+       private readonly ClienteContext _context; 
+
+       public UbicacionesController(ClienteContext context){
+           _context = context;
+       }
+
+       public IActionResult Clientes(){
+           var clientes = _context.Clientes.OrderBy(r => r.Nombre).ToList();
+           return View();
+       }
     }
 }
